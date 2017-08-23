f5f09989f05028c64f57bb105715746502800b9f
thainaspires <thainaspiress2@gmail.com>
2017-08-23 18:21:35 -0300

    Quiz 002
    Quiz pt2 -firebase OK
    quiz pt2 - answer OK

---
+++ app/src/main/java/com/ramoieee/wolfbyte/brunapplication/Questions.java
@@ -0,0 +1,10 @@
+package com.ramoieee.wolfbyte.brunapplication;
+
+public class Questions {
+    public String enunciado;
+    public String altA;
+    public String altB;
+    public String altC;
+    public String altD;
+    public String gabarito;
+}
--- app/src/main/java/com/ramoieee/wolfbyte/brunapplication/QuizActivity.java
+++ app/src/main/java/com/ramoieee/wolfbyte/brunapplication/QuizActivity.java
@@ -2,10 +2,16 @@ package com.ramoieee.wolfbyte.brunapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -13,7 +19,7 @@ public class QuizActivity extends AppCompatActivity {
     private TextView text_enunciado;
 
     String enunciado, altA, altB, altC, altD, gabarito;
-
+    int answerCount;
     DatabaseReference questionRef;
 
     @Override
@@ -30,23 +36,65 @@ public class QuizActivity extends AppCompatActivity {
 
         questionRef = FirebaseDatabase.getInstance().getReference("questions");
 
-        questionRef.child("Example").addValueEventListener(new ValueEventListener() {
+        questionRef.child("bio").child("Clonagem").addValueEventListener(new ValueEventListener() {
             @Override
+
             public void onDataChange(DataSnapshot dataSnapshot) {
-                enunciado = dataSnapshot.child("enunciado").getValue(String.class);
-                altA = dataSnapshot.child("altA").getValue(String.class);
-                altB = dataSnapshot.child("altB").getValue(String.class);
-                altC = dataSnapshot.child("altC").getValue(String.class);
-                altD = dataSnapshot.child("altD").getValue(String.class);
-                gabarito = dataSnapshot.child("gabarito").getValue(String.class);
+                long numberOfChildren = dataSnapshot.getChildrenCount();
+                ArrayList arrayChildren = new ArrayList();
+                for (int i = 1; i <= numberOfChildren; i++) {
+                    arrayChildren.add(i);
+                }
+                Collections.shuffle(arrayChildren);
 
-                text_enunciado.setText(enunciado);
-                button_altA.setText(altA);
-                button_altB.setText(altB);
-                button_altC.setText(altC);
-                button_altD.setText(altD);
+                for (int i = 0; i < arrayChildren.size(); i++) {
+                    enunciado = dataSnapshot.child(arrayChildren.get(i).toString()).child("enunciado").getValue(String.class);
+                    altA = dataSnapshot.child(arrayChildren.get(i).toString()).child("altA").getValue(String.class);
+                    altB = dataSnapshot.child(arrayChildren.get(i).toString()).child("altB").getValue(String.class);
+                    altC = dataSnapshot.child(arrayChildren.get(i).toString()).child("altC").getValue(String.class);
+                    altD = dataSnapshot.child(arrayChildren.get(i).toString()).child("altD").getValue(String.class);
+                    gabarito = dataSnapshot.child(arrayChildren.get(i).toString()).child("gabarito").getValue(String.class);
+
+                    text_enunciado.setText(enunciado);
+                    button_altA.setText(altA);
+                    button_altB.setText(altB);
+                    button_altC.setText(altC);
+                    button_altD.setText(altD);
+
+                    button_altA.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (gabarito.equals("AltA")) {
+                                answerCount++;
+                            }
+                        }
+                    });
+                    button_altB.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (gabarito.equals("AltB")) {
+                                answerCount++;
+                            }
+                        }
+                    });
+                    button_altC.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (gabarito.equals("AltC")) {
+                                answerCount++;
+                            }
+                        }
+                    });
+                    button_altD.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (gabarito.equals("AltD")) {
+                                answerCount++;
+                            }
+                        }
+                    });
+                }
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
