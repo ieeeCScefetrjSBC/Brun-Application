d3d92cbbc059ebf7f68462f24ed95ba04608653e
thainaspires <thainaspiress2@gmail.com>
2017-09-03 23:46:09 -0300

    Quiz 004
    Quiz pt3 - firebase OK
    Quiz pt3 - Acrescentado Alt E
    Quiz pt3 - Método para setar o índice em qualquer local
    Quiz pt3 - Método para setar o answerCount em qualquer local

--- app/src/main/java/com/ramoieee/wolfbyte/brunapplication/Questions.java
+++ app/src/main/java/com/ramoieee/wolfbyte/brunapplication/Questions.java
@@ -6,6 +6,6 @@ public class Questions {
     public String altB;
     public String altC;
     public String altD;
+    public String altE;
     public String gabarito;
-
 }
--- app/src/main/java/com/ramoieee/wolfbyte/brunapplication/QuizActivity.java
+++ app/src/main/java/com/ramoieee/wolfbyte/brunapplication/QuizActivity.java
@@ -4,47 +4,50 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.firebase.database.*;
-
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Map;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private Button button_altA, button_altB, button_altC, button_altD;
+    private Button button_altA, button_altB, button_altC, button_altD, button_altE;
     private TextView text_enunciado;
 
-    String enunciado, altA, altB, altC, altD, gabarito;
+    String enunciado, altA, altB, altC, altD, altE, gabarito;
     int answerCount;
 
     DatabaseReference questionRef;
     ArrayList arrayChildren = new ArrayList(); //Array para colocar todas as perguntas e suas alternativas/gabarito
     Long numberOfChildren; //Váriavel pra colocar quantas questões tem
-    int ind = 0; //Váriavel para saber a pergunta de qual indice puxar do array
+    int ind; //Váriavel para saber a pergunta de qual indice puxar do array
 
-    public void ExibirPergunta(int index){
+    public void setInd(int ind){
+        this.ind = ind; //método para mudar o valor de ind em qualquer lugar da classe
+    }
+    public void setAnswerCount(int answerCount){
+        this.answerCount = answerCount; //método para mudar o valor de answerCount em qualquer lugar da classe
+    }
+    public void ExibirPergunta(int index){ //Método para exibir as perguntas
         if(index < numberOfChildren) {
-            Questions teste = (Questions) arrayChildren.get(index);
-            enunciado = teste.enunciado;
-            altA = teste.altA;
-            altB = teste.altB;
-            altC = teste.altC;
-            altD = teste.altD;
-            gabarito = teste.gabarito;
+            Questions ques = (Questions) arrayChildren.get(index);
+            enunciado = ques.enunciado;
+            altA = ques.altA;
+            altB = ques.altB;
+            altC = ques.altC;
+            altD = ques.altD;
+            altE = ques.altE;
+            gabarito = ques.gabarito;
             text_enunciado.setText(enunciado);
             button_altA.setText(altA);
             button_altB.setText(altB);
             button_altC.setText(altC);
             button_altD.setText(altD);
+            button_altE.setText(altE); //Ascrescentei a alternativa E, que não tinha antes
         }
-        else{ //Se o index for igual ao número de perguntas, é porque chegou ao final e passa pra activity das matérias
+        else{ //Se o index for igual ao número de perguntas, é porque chegou ao final e passa pra activity das matérias (provisório)
             Intent int_quiz_example = new Intent(QuizActivity.this, SubjectsActivity.class);
             startActivity(int_quiz_example);
         }
@@ -57,6 +60,7 @@ public class QuizActivity extends AppCompatActivity {
         button_altB = (Button)findViewById(R.id.button_altB);
         button_altC = (Button)findViewById(R.id.button_altC);
         button_altD = (Button)findViewById(R.id.button_altD);
+        button_altE = (Button)findViewById(R.id.button_altE);
 
         text_enunciado = (TextView)findViewById(R.id.text_enunciado);
 
@@ -71,6 +75,8 @@ public class QuizActivity extends AppCompatActivity {
                     arrayChildren.add(itemSnapshot2.getValue(Questions.class)); //adiciona cada questão no array
                 }
                 Collections.shuffle(arrayChildren); //Embaralha array
+                setAnswerCount(0); //Faz o answerCount ser 0, provisório
+                setInd(0);//Faz o ind ser 0, provisório
                 ExibirPergunta(ind); //Exibe pela primeira vez a primeira pergunta
             }
             @Override
@@ -84,45 +90,67 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) { /*se clicar no 1º botão, vê se a resposta está certa, incrementa a variavel answerCount e chama
                 a função ExibirPergunta novamente, mas passando o índice incrementado para q exiba a próxima.  */
                 Questions t = (Questions) arrayChildren.get(ind);
-                if (t.gabarito.equals("AltA")) { //não está entrando no equals, precisa resolver isso
-                    answerCount++;
+                String gab = t.gabarito; //Coloca o gabarito na variável gab, não tava certo fazendo direto então coloquei na variável
+                if (gab.equals("altA")) {
+                    setAnswerCount(answerCount+1);
                     System.out.println(answerCount);
                 }
-                ExibirPergunta(ind++);
+                setInd(ind+1); //aumenta o índice
+                ExibirPergunta(ind);
             }
         });
+
         //Utilizando o mesmo raciocinio do primeiro botão, os outros botões também fazem o mesmo
         button_altB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Questions t = (Questions) arrayChildren.get(ind);
-                if (t.gabarito.equals("AltB")) {
-                    answerCount++;
+                String gab = t.gabarito;
+                if (gab.equals("altB")) {
+                    setAnswerCount(answerCount+1);
                     System.out.println(answerCount);
                 }
-                ExibirPergunta(ind++);
+                setInd(ind+1);
+                ExibirPergunta(ind);
             }
         });
         button_altC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Questions t = (Questions) arrayChildren.get(ind);
-                if (t.gabarito.equals("AltC")) {
-                    answerCount++;
+                String gab = t.gabarito;
+                if (gab.equals("altC")) {
+                    setAnswerCount(answerCount+1);
                     System.out.println(answerCount);
                 }
-                ExibirPergunta(ind++);
+                setInd(ind+1);
+                ExibirPergunta(ind);
             }
         });
         button_altD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Questions t = (Questions) arrayChildren.get(ind);
-                if (t.gabarito.equals("AltD")) {
-                    answerCount++;
+                String gab = t.gabarito;
+                if (gab.equals("altD")) {
+                    setAnswerCount(answerCount+1);
                     System.out.println(answerCount);
                 }
-                ExibirPergunta(ind++);
+                setInd(ind+1);
+                ExibirPergunta(ind);
+            }
+        });
+        button_altE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Questions t = (Questions) arrayChildren.get(ind);
+                String gab = t.gabarito;
+                if (gab.equals("altE")) {
+                    setAnswerCount(answerCount+1);
+                    System.out.println(answerCount);
+                }
+                setInd(ind+1);
+                ExibirPergunta(ind);;
             }
         });
 
