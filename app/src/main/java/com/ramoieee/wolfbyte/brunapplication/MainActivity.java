0fc0c5a468c45d6414e31e04976532ffb3efcb7d
Felipe Florentino Lisbôa <felipe.florentino.lisboa@gmail.com>
2017-10-21 22:39:35 -0200

    Alterações para área dos professores e acesso a dados dos alunos

--- app/src/main/java/com/ramoieee/wolfbyte/brunapplication/MainActivity.java
+++ app/src/main/java/com/ramoieee/wolfbyte/brunapplication/MainActivity.java
@@ -10,18 +10,27 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button button_quiz, button_settings;
     TextView view_welcome_text;
-
+    String access;
+    UserInfo myUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
 
 
         button_quiz = (Button) findViewById(R.id.button_quiz);
@@ -38,6 +47,21 @@ public class MainActivity extends AppCompatActivity {
             startActivity(int_SignIn);
             finish();
         } else {
+            myRef.child(user.getUid().toString()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()){
+                        myUser = dataSnapshot.getValue(UserInfo.class);
+                        access = myUser.credencial;
+                        //showUserInt(access);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
             Toast.makeText(MainActivity.this, "Usuário conectado",
                     Toast.LENGTH_SHORT).show();
             view_welcome_text.setText("Olá, " + user.getDisplayName());
@@ -48,7 +72,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent int_subject = new Intent(MainActivity.this, SubjectsActivity.class);
                 startActivity(int_subject);
-                finish();
             }
         });
 
@@ -57,7 +80,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent int_UserSettings = new Intent(MainActivity.this, UserSettingsActivity.class);
                 startActivity(int_UserSettings);
+                finish();
             }
         });
+    }
+
+    void showUserInt(String s){
+        switch (s){
+            case "admin":
+                break;
+
+            case "professor":
+                button_quiz.setVisibility(View.GONE);
+                break;
+
+            case "aluno":
+                // botão de acesso ao quiz
+                // botão de acesso ao perfil
+                break;
+
+        }
+
     }
 }
