b73e800f3b7128f5006df6cab9e925ba8bac056a
sergiolcc <sergiolccastilho@gmail.com>
2017-12-05 23:22:43 -0200

    Exibição Activity mudanças
    Colocado data snapshot para puxar da firebase a pontuação dos alunos e
    colocado na progress bar.

--- app/src/main/java/com/ramoieee/wolfbyte/brunapplication/ExibicaoActivity.java
+++ app/src/main/java/com/ramoieee/wolfbyte/brunapplication/ExibicaoActivity.java
@@ -7,6 +7,13 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Collections;
+
 /**
  * Created by Sergio on 15/11/2017.
  */
@@ -23,12 +30,38 @@ public class ExibicaoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_exibicao);
 
         ProgressBar resultadoProgressBar=(ProgressBar)findViewById(R.id.progressBar);
-        resultPorcentagem=(TextView) findViewById(R.id.porcentagem);
+       // resultPorcentagem=(TextView) findViewById(R.id.porcentagem);
        // resultadoProgressBar.setMax(100);
-        //resultadoProgressBar.setProgress(35);
+        resultadoProgressBar.setProgress(80);
+        //String Turma = getIntent().getExtras().getString("turma");
+       // String aluno = getIntent().getExtras().getString("aluno");
 
-        int progressValue = resultadoProgressBar.getProgress();
-        resultPorcentagem.setText("80%");
+       int progressValue = resultadoProgressBar.getProgress();
+        resultPorcentagem.setText("80"+"%");
+
+       /*ptsRef = FirebaseDatabase.getInstance().getReference("pontos");
+
+        ptsRef.child(Turma).child(aluno).addValueEventListener(new ValueEventListener() {
+            @Override
+
+            public void onDataChange(DataSnapshot dataSnapshot) {
+        progressoAtual = dataSnapshot.getValue("indeterminado".class);
+        resultadoProgressBar.setProgress(progressoAtual);
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });*/
+
+
+
+
+
+
+
+
 
 
 
